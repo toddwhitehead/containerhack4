f5cfe3e4a7f083d0b8bb6f119b4024789ce0dc28
Nick Ward <nickward@microsoft.com>
2018-06-20 09:38:52 +1000

    added k8s api project

---
+++ WebApi2/Controllers/MinecraftServerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapp.Models;
+using k8s;
+
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MinecraftServerController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(config);
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
